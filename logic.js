b74521c9127e6e00c58ae60bd4c827a7f64e9fea
Christian Grimm <grimmchristian@gmx.de>
2024-10-04 18:30:07 +0200

    new build of the logic

--- logic.js
+++ logic.js
@@ -14,129 +14,106 @@ function divide (a, b) {
     return Number(a) / Number(b);
 }
 
-function operate (cacheNumber, workingNumber, operator) {
-    console.log(cacheNumber, workingNumber, operator)
+function operate (number1, number2, operator) {
+    console.log(number1, number2, operator)
     let result = ""
     switch (operator) {
         case "+":
-            result = add (cacheNumber, workingNumber);
+            result = add (number1, number2);
             break;
         case "-":
-            result = substract (cacheNumber, workingNumber);
+            result = substract (number1, number2);
             break;
         case "*":
-            result = multiply (cacheNumber, workingNumber);
+            result = multiply (number1, number2);
             break;
         case "/":
-            result = divide (cacheNumber, workingNumber);
+            result = divide (number1, number2);
             break;
     }
     return result;
 }
 
 
-function mainProcess(inpVal, keyType) {
-    if (keyType === "operator" && calcValues.workingNumber === "") 
-        return;
-    else if (keyType === "operator" && calcValues.workingNumber != "")
-        calcValues.stage++;
-    else {
+function mainProcess(inpVal) {
 
-
-        switch (calcValues.stage) {
-            case 0:
-            case 2:
-                updateValues(
-                    calcValues.cacheNumber, // cache
-                    calcValues.workingNumber += inpVal, // work
-                    calcValues.operator, // operator
-                    calcValues.stage, // stage
-                    calcValues.workingNumber, //dspWork
-                    calcValues.displayHistory // dspHistory
-                );
-                printDisplay();
-                break;
-            case 1:
-                updateValues(
-                    calcValues.workingNumber, // cache
-                    "", // work
-                    inpVal, // operator
-                    2, // stage
-                    calcValues.displayWork,// dspWork
-                    `${calcValues.workingNumber} ${inpVal}`,// dspHistory
-                );
-                printDisplay();
-                break;
-            case 3:
-                const calculation = operate(
-                    calcValues.workingNumber, 
-                    calcValues.cacheNumber, 
-                    calcValues.operator
-                );
-                const temp = calcValues.workingNumber
-
-                updateValues(
-                    calculation,// cache
-                    "",// work
-                    inpVal,// operator
-                    0, // stage
-                    calculation,// dspWork
-                    `${calcValues.displayHistory} ${temp} =` ,// dspHistory
-                )
-                printDisplay();
-                break;
+    if(cval.state === 0) {
+        if(arithmetics.includes(inpVal) && cval.number1 === "") { 
+            // forbit operator as very fist input
+            return null; 
+        } else if(arithmetics.includes(inpVal) && cval.number1 != "") {
+            cval.state = 1; 
+            cval.operator = inpVal; 
+        } else if(numbers.includes(inpVal)) { 
+            cval.number1 += inpVal;
+            cval.disMain = cval.number1;
+            printDisplay();
         }
     }
-    console.log(calcValues)
+
+    if(cval.state === 1) {
+        cval.disUpper = cval.number1;
+        if(numbers.includes(inpVal)) {
+            cval.state = 2;
+            cval.number2 += inpVal;
+        } else {
+            cval.operator = inpVal;      
+        }
+        cval.disUpper = `${cval.number1} ${cval.operator}`
+        printDisplay();
+    }   
     
-
-}
-
-function updateValues(cache, working, operator, stage, dspWork, dspHist) {
-    calcValues.cacheNumber = cache;
-    calcValues.workingNumber = working;
-    calcValues.operator = operator;
-    calcValues.stage = stage;
-    calcValues.displayWork = dspWork;
-    calcValues.displayHistory = dspHist;
-   
+    if(cval.state === 2) {
+        if(arithmetics.includes(inpVal)) {
+            console.log("wuff")
+        }
+        
+    }
+    
+    
+    console.log(cval);
 }
 
 function printDisplay() {
-    displayWork.innerText = calcValues.displayWork;
-    displayHistory.innerText = calcValues.displayHistory;
+    displayWork.innerText = cval.disMain;
+    displayHistory.innerText = cval.disUpper;
 };
 
 
 
 // declaring objects and globals
-const calcValues = {
-    stage: 0,
-    cacheNumber: "",
-    workingNumber: "",
+const cval = {
+    state: 0,
+    number1: "",
+    number2: "",
     operator: "",
-    displayWork: "",
-    displayHistory: "",
+    disMain: "",
+    disUpper: "",
     
 }
+
+let expression = "";
 
 const displayWork = document.querySelector("#display-working");
 const displayHistory = document.querySelector("#display-history");
 const numberKeys = document.querySelectorAll(".numberKey");
 const operatorKeys = document.querySelectorAll(".operatorKey");
 
+const arithmetics = ["+", "-", "*", "/", "="];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const special = ["clear"];
 
 numberKeys.forEach((numberKey) => {
     numberKey.addEventListener(
-        "click", () => mainProcess(numberKey.innerText, "number"))
-})
+        "click", () => mainProcess(numberKey.innerText, cval.state)
+    );
+});
 
 operatorKeys.forEach((operatorKey) => {
     operatorKey.addEventListener(
-        "click", () => mainProcess(operatorKey.innerText, "operator")
-    )
-})
+        "click", () => mainProcess(operatorKey.innerText, cval.state)
+    );
+});
 
 // initialize display
-updateValues("history", "", "", 0, "", "");
-printDisplay();
+printDisplay(cval.disMain, cval.disUpper);
