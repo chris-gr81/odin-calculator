917be3cd617f3f0fbd84b1aa942a9352378c7bf4
Christian Grimm <grimmchristian@gmx.de>
2024-10-04 15:27:02 +0200

    Get logic

--- logic.js
+++ logic.js
@@ -1,20 +1,21 @@
 function add (a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function substract (a, b) {
-    return a - b;
+    return Number(a) - Number(b);
 }
 
 function multiply (a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide (a, b) {
-    return a / b;
+    return Number(a) / Number(b);
 }
 
 function operate (cacheNumber, workingNumber, operator) {
+    console.log(cacheNumber, workingNumber, operator)
     let result = ""
     switch (operator) {
         case "+":
@@ -33,23 +34,109 @@ function operate (cacheNumber, workingNumber, operator) {
     return result;
 }
 
-function getInput(inpVal, keyType) {
-    workingNumber += inpVal;
-    displayWork.innerText = workingNumber;
-}
-let cacheNumber = ""; 
-let workingNumber = ""; 
-let operator = "";
 
-let displayWork = document.querySelector("#display-working");
-let numberKeys = document.querySelectorAll(".numberKey");
+function mainProcess(inpVal, keyType) {
+    if (keyType === "operator" && calcValues.workingNumber === "") 
+        return;
+    else if (keyType === "operator" && calcValues.workingNumber != "")
+        calcValues.stage++;
+    else {
+
+
+        switch (calcValues.stage) {
+            case 0:
+            case 2:
+                updateValues(
+                    calcValues.cacheNumber, // cache
+                    calcValues.workingNumber += inpVal, // work
+                    calcValues.operator, // operator
+                    calcValues.stage, // stage
+                    calcValues.workingNumber, //dspWork
+                    calcValues.displayHistory // dspHistory
+                );
+                printDisplay();
+                break;
+            case 1:
+                updateValues(
+                    calcValues.workingNumber, // cache
+                    "", // work
+                    inpVal, // operator
+                    2, // stage
+                    calcValues.displayWork,// dspWork
+                    `${calcValues.workingNumber} ${inpVal}`,// dspHistory
+                );
+                printDisplay();
+                break;
+            case 3:
+                const calculation = operate(
+                    calcValues.workingNumber, 
+                    calcValues.cacheNumber, 
+                    calcValues.operator
+                );
+                const temp = calcValues.workingNumber
+
+                updateValues(
+                    calculation,// cache
+                    "",// work
+                    inpVal,// operator
+                    0, // stage
+                    calculation,// dspWork
+                    `${calcValues.displayHistory} ${temp} =` ,// dspHistory
+                )
+                printDisplay();
+                break;
+        }
+    }
+    console.log(calcValues)
+    
+
+}
+
+function updateValues(cache, working, operator, stage, dspWork, dspHist) {
+    calcValues.cacheNumber = cache;
+    calcValues.workingNumber = working;
+    calcValues.operator = operator;
+    calcValues.stage = stage;
+    calcValues.displayWork = dspWork;
+    calcValues.displayHistory = dspHist;
+   
+}
+
+function printDisplay() {
+    displayWork.innerText = calcValues.displayWork;
+    displayHistory.innerText = calcValues.displayHistory;
+};
+
+
+
+// declaring objects and globals
+const calcValues = {
+    stage: 0,
+    cacheNumber: "",
+    workingNumber: "",
+    operator: "",
+    displayWork: "",
+    displayHistory: "",
+    
+}
+
+const displayWork = document.querySelector("#display-working");
+const displayHistory = document.querySelector("#display-history");
+const numberKeys = document.querySelectorAll(".numberKey");
+const operatorKeys = document.querySelectorAll(".operatorKey");
+
 
 numberKeys.forEach((numberKey) => {
     numberKey.addEventListener(
-        "click", () => getInput(numberKey.innerText, "number"))
+        "click", () => mainProcess(numberKey.innerText, "number"))
 })
 
-console.log(display.innerText)
-// console.log(substract(a, b));
-// console.log(mulitply(a, b));
-// console.log(divide(a, b));
+operatorKeys.forEach((operatorKey) => {
+    operatorKey.addEventListener(
+        "click", () => mainProcess(operatorKey.innerText, "operator")
+    )
+})
+
+// initialize display
+updateValues("history", "", "", 0, "", "");
+printDisplay();
